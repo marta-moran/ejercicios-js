1b31de70bd5fe5e99f3496444e5642a8bdedfb12
marta <martamoranl06@gmail.com>
2022-02-16 18:00:17 +0100

    nuevos ejercicios examen

---
+++ Ajax/ejemplo-aleatorioSINO/ajax.php
@@ -0,0 +1,11 @@
+<?php
+
+$aleatorio=rand(0,10);
+
+if($aleatorio%2==0){
+	echo "Si";
+}else{
+
+	echo "No";
+}
+?>
---
+++ Ajax/ejemploExamen/datos.php
@@ -0,0 +1,45 @@
+﻿<?php
+// Array con nombres
+$a[1] = "Arantzazu";
+$a[2] = "Mónica";
+$a[3] = "Gema";
+$a[4] = "María";
+$a[5] = "Esla";
+$a[6] = "Ismael";
+$a[7] = "Jesús";
+$a[8] = "Adrián";
+$a[9] = "José";
+$a[10] = "Luisa";
+$a[11] = "Manuel";
+$a[12] = "Pablo";
+$a[13] = "Diego";
+$a[14] = "Ricardo";
+$a[15] = "Alonso";
+$a[16] = "Carlos";
+$a[17] = "Jacinto";
+$a[18] = "Ana";
+
+
+// Parámetro para buscar
+$q = $_REQUEST["buscador"];
+
+$resultado = "";
+
+// Comprobación
+if ($q !== "") {
+    $q = strtolower($q);
+    $tam=strlen($q);
+    foreach($a as $nombre) {
+        if (stristr($q, substr($nombre, 0, $tam))) {
+            if ($resultado === "") {
+                $resultado = "<p>".$nombre."<p>";
+            } else {
+                $resultado .= "<p>".$nombre."<p>";
+            }
+        }
+    }
+}
+
+// Mensaje si no hay resultados
+echo $resultado === "" ? "no existen coincidencias " : $resultado;
+?>
